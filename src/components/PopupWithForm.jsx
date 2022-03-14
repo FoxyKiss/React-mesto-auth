e1f15dcaacdf2ab17b46229996480f1cd1e354ac
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-03-14 16:37:05 +0600

    "fix: Fix adding listeners every time Popup is opened"

--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
 export default function PopUpWithForm({ formProps, isOpen, onClose, children, onSubmit }) {
-  const popupRef = React.useRef({})
-
   //? Закрытие модалки при клике на оверлей
   React.useEffect(() => {
-    popupRef.current.addEventListener('click', (evt) => {
+    function clickOverlayClose(evt) {
       if (evt.target.classList.contains('popup_open')) {
         onClose()
       }
-    })
+    }
+    document.addEventListener("click", clickOverlayClose);
+
+    return () => document.removeEventListener("click", clickOverlayClose);
   }, [])
 
   //? Основная разметка Popup
   return (
-    <div ref={popupRef} className={`popup popup_${formProps.name} ${isOpen && 'popup_open'} `}>
+    <div className={`popup popup_${formProps.name} ${isOpen && 'popup_open'} `}>
       <div className="popup__container">
         <h2 className="popup__title">{formProps.title}</h2>
         <form id={formProps.formId} className="popup__form" onSubmit={onSubmit} >
